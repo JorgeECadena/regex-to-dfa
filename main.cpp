dca1029ee104f1639a466286ac54353b46b57dd3
Jorge Cadena <a01660252@tec.mx>
2025-01-17 23:45:01 -0600

    Created new set 'alpha' and modified function to work with it

--- main.cpp
+++ main.cpp
@@ -12,19 +12,19 @@
  *
  * @return bool
  * */
-bool validateAlphabet(const std::string& alphabet, std::unordered_set<char>& vC) {
+bool validateAlphabet(const std::string& alphabet, std::unordered_set<char>& alpha) {
 	for (char c : alphabet) {
 		if (!isalpha(c)) {
 			std::cerr << "Character: " << c << " is not valid!\n";
 			return false;
 		}
 
-		if (vC.find(c) != vC.end()) {
+		if (alpha.find(c) != alpha.end()) {
 			std::cerr << "Character: " << c << " cannot be repeated!\n";
 			return false;
 		}
 
-		vC.insert(c);
+		alpha.insert(c);
 	}
 
 	return true;
@@ -38,6 +38,7 @@ int main() {
 		'*',
 		'|'
 	};
+	std::unordered_set<char> alpha;
 
 	std::string alphabet;
 	std::string regex;
@@ -45,8 +46,11 @@ int main() {
 	std::cout << "\nAlphabet: ";
 	std::cin >> alphabet;
 
-	if (!validateAlphabet(alphabet, validChars))
+	if (!validateAlphabet(alphabet, alpha))
 		return 1;
+
+	std::cout << "RegEx: ";
+	std::cin >> regex;
 
 	return 0;
 }
