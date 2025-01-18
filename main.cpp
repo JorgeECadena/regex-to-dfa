62da60b5d0a96f19077d1a3ebdea2f6ceb2d4d19
Jorge Cadena <a01660252@tec.mx>
2025-01-18 00:22:29 -0600

    Created convert() function to transform a char into a state

--- main.cpp
+++ main.cpp
@@ -30,6 +30,39 @@ bool validateAlphabet(const std::string& alphabet, std::unordered_set<char>& alp
 	return true;
 }
 
+/**
+ * @brief Return a state depending on the received character's value
+ *
+ * @param const& char c Character to validate
+ * @param const std::unordered_set<char>& alpha Alphabet of the expression
+ *
+ * @return unsigned short
+ * */
+unsigned short convert(const char& c, const std::unordered_set<char>& alpha) {
+	// State 0 is for any character that's part of the alpha set
+	// State 1 is for '*' or '+' characters only
+	// State 2 is for '|' character
+	// State 3 is for '(' character
+	// State 4 is for ')' character
+	// State 5 is for ' ' or '\0' characters
+	// State 6 is for any other character not displayed here
+	if(alpha.find(c) != alpha.end()) return 0;
+	switch (c) {
+		case '*' :
+		case '+' : return 1;
+		case '|' : return 2;
+		case '(' : return 3;
+		case ')' : return 4;
+		case ' ' : 
+		case '\0': return 5;
+		default  : return 6;
+	}
+}
+
+bool validateRegex(const std::string& regex) {
+	return true;
+}
+
 int main() {
 	std::unordered_set<char> validChars = {
 		'(', 
@@ -41,7 +74,7 @@ int main() {
 	std::unordered_set<char> alpha;
 
 	std::string alphabet;
-	std::string regex;
+	char regex[256];
 
 	std::cout << "\nAlphabet: ";
 	std::cin >> alphabet;
